debe9edc6ed7e0161db6028f8121807404b5f72f
Luan Eleuterio <luaneleuterio3@hotmail.com>
2021-04-27 18:24:53 -0300

    Ajustado alguns fontes

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.connect(
-    'mongodb+srv://luan:luan30798@juno-integration.kjl52.mongodb.net/marketplace?retryWrites=true&w=majority',
+    '',
     { useNewUrlParser: true, useUnifiedTopology: true },
 )
 mongoose.Promise = global.Promise
