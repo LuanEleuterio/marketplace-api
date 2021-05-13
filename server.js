188b991ae6def64c45e0d64956e42c625c7dc55c
Luan Eleuterio <luaneleuterio3@hotmail.com>
2021-05-12 23:32:09 -0300

    Adicionado variavel de ambiente PORT para o Heroku

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.use(cors())
 app.use(express.json());
 require("./routes/index.router")(app);
 
-app.listen(8080, function () {
+app.listen(process.env.PORT || 8080, function () {
     console.log("CORS-enabled web server listening on port 8080");
 });
